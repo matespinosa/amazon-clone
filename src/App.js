2aa1122e35c9e7bdba778cb39999af5aaec460dd
Mateo <matespinosa09@gmail.com>
2021-04-21 10:23:05 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Header from './Header';
 import Cart from './Cart';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Cart />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
